b75e3abd50769becf445431fe5f2e55a349941ea
Art Witczak <art.bikes@gmail.com>
2013-01-04 00:57:51 -0800

    got sqlite db creation going

--- app.rb
+++ app.rb
@@ -1,14 +1,26 @@
 require 'sinatra'
 
-
-
 class Application < Sinatra::Base
+
+	if ENV['VCAP_SERVICES'].nil?
+     DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/words.db")
+   else
+     require 'json'
+     svcs = JSON.parse ENV['VCAP_SERVICES']
+     mysql = svcs.detect { |k,v| k =~ /^mysql/ }.last.first
+     creds = mysql['credentials']
+     user, pass, host, name = %w(user password host name).map { |key| creds[key] }
+     DataMapper.setup(:default, "mysql://#{user}:#{pass}@#{host}/#{name}")
+   end
+
+
 	class Wordtype
+		include DataMapper::Resource
 		property :id,		Serial
 		property :word,		String
 	end
 	DataMapper.finalize
-	Word.auto_upgrade!
+	Wordtype.auto_upgrade!
 	class DbSeeds
 		def self.seed
 			DO IT
